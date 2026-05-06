737df2098eb286624c7e7962a37100d4f785ec05
Pragati Chauhan <pragatichauhan1206@gmail.com>
2026-05-06 18:47:25 +0530

    Update News API key in app.py
    Updated the News API key in app.py.

--- FlashFeed/backend/app.py
+++ FlashFeed/backend/app.py
@@ -14,7 +14,7 @@ except ImportError:
 app = Flask(__name__)
 CORS(app)  # allows frontend to talk to backend
 
-# 🔑 Replace with your NewsData.io API key
+
 NEWS_API_KEY = "pub_9a19043a64b345f7b01c608da01217cf"
 
 # Category keywords mapping
